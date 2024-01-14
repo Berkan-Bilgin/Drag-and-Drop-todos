9aa8ab8c42117f490e2f678334377d8c64389c0d
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-01-14 22:52:34 +0300

    feat: fix hydration error with useEffect hook

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,27 @@
 "use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Column from "./components/Column";
 import { useStore } from "@/store/taskStore";
 
 export default function Home() {
   const tasks = useStore((state) => state.tasks);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Bu kod bloğu yalnızca istemci tarafında çalışacak
+    setIsClient(true);
+  }, []);
   return (
     <>
-      <div className="App">
-        <Column state="PLANNED" />
-        <Column state="ONGOING" />
+      {isClient && (
+        <div className="App">
+          <Column state="PLANNED" />
+          <Column state="ONGOING" />
 
-        <Column state="DONE" />
-      </div>
-
-      <div className="bg-red-500"> {tasks[0].title}</div>
+          <Column state="DONE" />
+        </div>
+      )}
     </>
   );
 }
